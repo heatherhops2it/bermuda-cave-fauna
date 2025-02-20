a7c255fde46f94b1d778c6a82b4a37c656d94d79
heatherhops2it <heathergallowaybda@gmail.com>
2025-02-20 13:00:37 -0500

    loaded in data

---
+++ scripts/soniferous-families.R
@@ -0,0 +1,25 @@
+
+# load packages -----------------------------------------------------------
+
+library(tidyverse)
+
+
+
+# load data ---------------------------------------------------------------
+
+files <- list.files(path = "C:/Users/hng9/Box/Research - Endemics/Soniferous Marine Fauna/Underwater Sonifery Code/WoRMS Underwater Sonifery Code/Species from WoRMS",
+                    full.names = TRUE)
+
+df_son <- data_frame(NULL)
+df_null <- data_frame(NULL)
+i <- NULL
+
+for (i in files) {
+ df_null <- read_csv(file = i)
+ 
+ df_son <- df_son |> 
+   bind_rows(df_null)
+}
+
+
+bda_spp <- read_csv(file = "bermuda-species.csv")
