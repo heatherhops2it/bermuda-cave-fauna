13a22dc88fb5351755f5cc8907aa392d126e7fc2
heatherhops2it <heathergallowaybda@gmail.com>
2025-02-21 14:20:15 -0500

    added worrms package to search for classification info; created for loop to import classification info for each sp.

--- scripts/soniferous-families.R
+++ scripts/soniferous-families.R
@@ -2,6 +2,11 @@
 # load packages -----------------------------------------------------------
 
 library(tidyverse)
+library(readr)
+# library("worms")  # install.packages("worms")
+library(worrms)
+library(progress)  # install.packages("progress")
+
 
 
 
@@ -45,7 +50,51 @@ son_info <- df_son |>
 uk_son_spp <- son_spp |> 
   anti_join(df_son, by = "Genus")
 
+## make a df that has the geni not included on df_son:
+uk_son_gen <- uk_son_spp |> 
+  select(Genus, Species) |> 
+  mutate(Taxon = paste(Genus, Species)) |> 
+  mutate(AlphaID = wm_name2id_(Taxon))
+
+
+
 ## the uk_son_spp is the species that are labelled as soniferous but are not in the 'Species from WORMS' folder
+
+# create the progress bar
+n_iter <- 16279
+pb <- progress_bar$new(format = "(:spin) [:bar] :percent [Elapsed time: :elapsedfull || Estimated time remaining: :eta]",
+                       total = n_iter,
+                       complete = "=",   # Completion bar character
+                       incomplete = "-", # Incomplete bar character
+                       current = ">",    # Current bar character
+                       clear = FALSE,    # If TRUE, clears the bar when finish
+                       width = 200) 
+
+
+# set up dataframes for for loop
+df_tax <- data_frame(NULL)
+df_wo <- data_frame(NULL)
+
+for (i in 1:length(uk_son_gen$Genus)) {
+  # progress counter
+  pb$tick()
+  
+  # get the name of the species
+  name <- uk_son_gen$Genus[i]
+  
+  # use it in worrms functions to get taxon info, and save to 'writeover' dataframe
+  df_wo <- wm_classification(id = wm_name2id(name)) |> 
+    select(rank, scientificname) |> 
+    pivot_wider(names_from = rank, values_from = scientificname)
+  
+  # add it to the dataframe for taxonomy
+  df_tax <- df_tax |> 
+    bind_rows(df_wo)
+}
+
+
+
+
 
 
 
